39b61c9d6213dd358d92bc8f265c316be1dd222c
aqhlia <aqhlianrmdnhi@gmail.com>
2025-04-25 07:39:22 +0800

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <body>
+ 
+        <?php
+            // This is a single-line comment
+
+            # This is also a single-line comment
+
+            /* This is a multi-line comment */
+        ?>
+
+        <?php
+            echo "Welcome Home!"; // Outputs a welcome message
+        ?>
+ 
+    </body>
+</html>
